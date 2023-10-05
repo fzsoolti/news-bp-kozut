cc56b7efc5acdc467914159046b71d1195035c65
Fazekas Zsolt <fzsolti1997@gmail.com>
2023-10-05 17:42:58 +0200

    post details

---
+++ newsApp/newsApp-client/src/app/components/main/news-feed/post-detials/post-detials.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-detials',
+  templateUrl: './post-detials.component.html',
+  styleUrls: ['./post-detials.component.css']
+})
+export class PostDetialsComponent {
+
+}
